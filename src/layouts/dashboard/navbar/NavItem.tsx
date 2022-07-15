313f4256ed57ce0c647c4a40be66b5d1c6eea486
snowingfox <1503401882@qq.com>
2022-07-15 10:50:50 +0800

    :art: 优化代码结构

--- src/layouts/dashboard/navbar/NavItem.tsx
+++ src/layouts/dashboard/navbar/NavItem.tsx
@@ -1,8 +1,8 @@
 import { NavLink as RouterLink, useLocation } from 'react-router-dom'
-import { Box, Collapse, List, ListItemText, useTheme } from '@mui/material'
+import { useTheme } from '@mui/material'
 import { ListItemIconStyle, ListItemStyle, ListItemTextStyle } from './style'
 import type { CustomThemeOptions } from '@/theme/overrides'
-import type { TNavListConfig, TNavListItem } from '@/layouts/dashboard/navbar/navListConfig'
+import type { TNavListItem } from '@/layouts/dashboard/navbar/navListConfig'
 import { getActive } from '@/layouts/dashboard/navbar/utils'
 
 export function NavListItemRoot({ item }: { item: TNavListItem }) {
@@ -24,95 +24,3 @@ export function NavListItemRoot({ item }: { item: TNavListItem }) {
   </ListItemStyle>
 }
 
-function NavListSub({ list }: { list: any }) {
-  const { pathname } = useLocation()
-
-  const active = getActive(list.path, pathname)
-
-  const [open, setOpen] = useState(active)
-
-  const hasChildren = list.children
-
-  if (hasChildren) {
-    return (
-      <>
-        <NavItemSub item={list} onOpen={() => setOpen(!open)} open={open} active={active} />
-
-        <Collapse in={open} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding sx={{ pl: 3 }}>
-            {(list.children || []).map(item => (
-              <NavItemSub key={item.title} item={item} active={getActive(item.path, pathname)} />
-            ))}
-          </List>
-        </Collapse>
-      </>
-    )
-  }
-
-  return <NavItemSub item={list} active={active} />
-}
-
-interface NavItemRootProps {
-  item: TNavListConfig
-  open: boolean
-  active: boolean
-}
-
-const onOpen = () => {}
-
-export function NavItemRoot({ item, open = false, active }: NavItemRootProps) {
-
-}
-
-export function NavItemSub({ item, open = false, active = false, onOpen }) {
-  const { title, path, info, children } = item
-
-  const renderContent = (
-    <>
-      <DotIcon active={active} />
-      <ListItemText disableTypography primary={title} />
-      {info && info}
-      {children && <ArrowIcon open={open} />}
-    </>
-  )
-
-  if (children) {
-    return (
-      <ListItemStyle onClick={onOpen} activeSub={active} subItem activeRoot={false}>
-        {renderContent}
-      </ListItemStyle>
-    )
-  }
-
-  return <ListItemStyle component={RouterLink} to={path} activeSub={active} subItem>
-    {renderContent}
-  </ListItemStyle>
-}
-
-export function DotIcon({ active }) {
-  return (
-    <ListItemIconStyle>
-      <Box
-        component="span"
-        sx={{
-          width: 4,
-          height: 4,
-          borderRadius: '50%',
-          bgcolor: 'text.disabled',
-          transition: theme =>
-            theme.transitions.create('transform', {
-              duration: theme.transitions.duration.shorter,
-            }),
-          ...(active && {
-            transform: 'scale(2)',
-            bgcolor: 'primary.main',
-          }),
-        }}
-      />
-    </ListItemIconStyle>
-  )
-}
-
-export function ArrowIcon({ open }: { open: boolean }) {
-  return <i className={open ? 'i-eva:arrow-ios-downward-fill' : 'i-eva:arrow-ios-forward-fill'}/>
-}
--- src/layouts/dashboard/navbar/style.ts
+++ src/layouts/dashboard/navbar/style.ts
@@ -1,13 +1,12 @@
 import { alpha, styled } from '@mui/material/styles'
-import type { ListItemButtonProps } from '@mui/material'
 import { ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import { NAVBAR } from '@/shared/constant/ui'
 import type { CustomThemeOptions } from '@/theme/overrides'
-import type { ReactNode } from 'react'
+import type { NavLinkProps } from 'react-router-dom'
 
 export const ListItemStyle = styled(ListItemButton, {
   shouldForwardProp: prop => prop !== 'activeRoot' && prop !== 'activeSub' && prop !== 'subItem',
-})(({ activeRoot, activeSub, subItem, theme }: { activeRoot: boolean; activeSub: boolean; subItem: boolean; theme?: CustomThemeOptions; component: ReactNode }) => {
+})(({ activeRoot, theme }: { activeRoot: boolean; theme?: CustomThemeOptions; component: any } & Partial<NavLinkProps>) => {
   theme = theme!
   return {
     ...theme.typography.body2,
@@ -19,20 +18,10 @@ export const ListItemStyle = styled(ListItemButton, {
     marginBottom: theme.spacing(0.5),
     color: theme.palette.text.secondary,
     borderRadius: theme.shape.borderRadius,
-    // activeRoot
     ...(activeRoot && {
       ...theme.typography.subtitle2,
       color: theme.palette.primary.main,
       backgroundColor: alpha(theme.palette.primary.main, theme.palette.action.selectedOpacity),
-    }),
-    // activeSub
-    ...(activeSub && {
-      ...theme.typography.subtitle2,
-      color: theme.palette.text.primary,
-    }),
-    // subItem
-    ...(subItem && {
-      height: NAVBAR.DASHBOARD_ITEM_SUB_HEIGHT,
     }),
   }
 })
