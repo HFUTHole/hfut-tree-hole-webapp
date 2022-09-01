c2a1f6795173ca130d146d60918d91cc14cc0040
snowingfox <1503401882@qq.com>
2022-09-01 15:48:04 +0800

    fix: fix type err

--- src/service/api/treehole.ts
+++ src/service/api/treehole.ts
@@ -69,7 +69,7 @@ export function postHoleMutation(content: string) {
   })
 }
 
-export function removeOrReportHoleMutation({ id, isReport = false }: { id: number; isReport: boolean }) {
+export function removeOrReportHoleMutation({ id, isReport = false }: { id: number; isReport?: boolean }) {
   return request({
     url: '/treehole/remove',
     method: 'DELETE',
